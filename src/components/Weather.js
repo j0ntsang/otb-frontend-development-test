eea6c3bf9de973d7f7e174baf76f5fd6987d25f0
Jonathan Tsang <>
2024-06-21 16:47:55 -0300

    capture dynamic weather results

---
+++ src/components/Weather.js
@@ -0,0 +1,103 @@
+"use client";
+
+import "./Weather.css";
+
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
+const cityOptions = ["Tokyo", "New York", "Moscow"];
+
+const Weather = () => {
+  const [city, setCity] = useState("Tokyo");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchWeather = async (city) => {
+    try {
+      const response = await axios.get(`/api/weather`, {
+        params: { city },
+      });
+      setWeather(response.data);
+      setError(null);
+    } catch (err) {
+      setWeather(null);
+      setError("Error fetching weather data");
+    }
+  };
+
+  useEffect(() => {
+    fetchWeather(city);
+  }, [city]);
+
+  useEffect(() => {
+    console.log(weather);
+  }, [weather]);
+
+  return (
+    <div className="weather-app flex flex-col">
+      <nav className="cities">
+        <ul className="cities-list list-none flex">
+          {cityOptions.map((city) => {
+            const buttonClasses = ["button--city-selected"];
+            return (
+              <li key={city} className="cities-list-item">
+                <button className={buttonClasses} onClick={() => setCity(city)}>
+                  {city}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <div className="weather-stats flex">
+        <section className="current-weather flex flex-col">
+          <h1
+            className="current-city flex flex-col flex-grow items-center justify-center"
+            aria-label="It is currently [CONDITION] in [CITY] at [TEMPERATURE] degrees"
+            aria-live="polite">
+            {city}
+          </h1>
+          <div className="current-weather-details flex justify-evenly">
+            <h2 className="current-temperature inline-flex flex-col items-center justify-center">
+              <span className="current-temperature-unit">
+                {weather ? weather.current.temp_c : ""}
+                <span className="degree-symbol">&#176;</span>
+              </span>
+              <span className="subheading">Current</span>
+            </h2>
+            <h3 className="current-condition inline-flex flex-col  items-center justify-center">
+              <span className="current-condition-icon"></span>
+              <span className="subheading">
+                CONDITION
+                {weather ? weather.current.condition.text : ""}
+              </span>
+            </h3>
+          </div>
+        </section>
+        <aside className="forecast flex-grow">
+          <ul className="forecast-list list-none flex flex-col justify-evenly">
+            {weather?.forecast &&
+              weather.forecast.forecastday.slice(1).map((day, index) => (
+                <li
+                  key={day.date_epoch}
+                  className="forecast-list-item m-0 p-0 inline-flex flex-col flex-grow items-center justify-center">
+                  <span className="forecast-day">
+                    {new Intl.DateTimeFormat("en-US", {
+                      weekday: "long",
+                    }).format(new Date(day.date_epoch * 1000))}
+                  </span>
+                  <span className="forecast-temperature">
+                    {day.day.maxtemp_c}
+                    <span className="degree-symbol">&#176;</span>
+                  </span>
+                </li>
+              ))}
+          </ul>
+        </aside>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
---
+++ src/pages/api/weather.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { city } = req.query;
+  const apiKey = "cd8e598e87c24262b3831328210203";
+
+  if (!city) {
+    return res.status(400).json({ error: "City parameter is required" });
+  }
+
+  try {
+    const response = await axios.get(
+      `https://api.weatherapi.com/v1/forecast.json`,
+      {
+        params: {
+          key: apiKey,
+          q: city,
+          days: 4,
+        },
+      }
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error fetching weather data" });
+  }
+}
